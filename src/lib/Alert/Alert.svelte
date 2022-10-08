60b54bafbab9e8cf266db0fb965e8b04c3f23b76
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-10-08 19:42:29 -0300

    Feat alert message with create and delete task
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/lib/Alert/Alert.svelte
+++ src/lib/Alert/Alert.svelte
@@ -8,7 +8,7 @@
   $: bgClass = () => {
     if ($alertType === ALERT_TYPE.SUCCESS) {
       return 'bg-green-100';
-    } else if ($alertType === ALERT_TYPE.DANGER) {
+    } else if ($alertType === ALERT_TYPE.DELETE) {
       return 'bg-red-100';
     } else {
       return 'bg-blue-100';
@@ -16,10 +16,10 @@
   };
 </script>
 
-<div class="h-10 my-6">
-  {#if alertMessage}
-    <span class={`text-gray-900 text-center rounded-md p-4 mb-4 ${bgClass()}`}
-      >{$alertMessage}</span
-    >
+<div class="w-11/12 mx-auto my-6 text-center md:w-1/4">
+  {#if $alertMessage}
+    <span class={`text-gray-900 text-center rounded-md p-4 ${bgClass()}`}
+      >{$alertMessage}
+    </span>
   {/if}
 </div>
--- src/stores/alertStore.js
+++ src/stores/alertStore.js
@@ -1,14 +1,14 @@
 import { writable } from 'svelte/store';
 
 export const ALERT_TYPE = {
-  DANGER: 'DANGER',
+  DELETE: 'DELETE',
   INFO: 'INFO',
   SUCCESS: 'SUCCESS',
 };
 
 Object.freeze(ALERT_TYPE);
 
-export const alertMessage = writable('fa');
+export const alertMessage = writable('');
 export const alertType = writable('');
 
 export const displayAlert = (message, type = ALERT_TYPE.INFO, resetTime) => {
